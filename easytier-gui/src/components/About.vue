3b9b07f24dfd592e9ce2f5c4566c3b0c3433fe2e
SoKingwah <peter446833245@gmail.com>
2026-08-24 22:58:00 +0800

    feat(anf): 关于弹窗同时展示客户端版本与 Core 版本

--- easytier-gui/src/components/About.vue
+++ easytier-gui/src/components/About.vue
@@ -1,24 +1,32 @@
 <script setup lang="ts">
 import { getEasytierVersion } from '~/composables/backend'
+import pkg from '~/../package.json'
+import { formatVersionDisplay } from '~/composables/version'
 
 const { t } = useI18n()
 
-const etVersion = ref('')
+const coreVersion = ref('')
 
 onMounted(async () => {
-  etVersion.value = await getEasytierVersion()
+  coreVersion.value = await getEasytierVersion()
 })
+
+const display = computed(() => formatVersionDisplay(pkg.version, coreVersion.value))
 </script>
 
 <template>
   <Card>
     <template #title>
-      ANF 平台架构 - {{ t('about.version') }}: {{ etVersion }}
+      ANF 平台架构
     </template>
     <template #content>
       <p class="mb-1">
         {{ t('about.description') }}
       </p>
+      <div class="mt-3 space-y-1 text-sm">
+        <p>{{ t('about.gui_version') }}: {{ display.gui }}</p>
+        <p>{{ t('about.core_version') }}: {{ display.core }}</p>
+      </div>
     </template>
   </Card>
 </template>
